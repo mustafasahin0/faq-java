0d01174d73cb3f781439dfe027ef907a851dffe6
msahin <msahin@switchfly.com>
2022-03-10 20:31:52 -0700

    added algorithm challange

--- src/main/java/algorithm/FindFirstNonRepeatingCharInString.java
+++ src/main/java/algorithm/FindFirstNonRepeatingCharInString.java
@@ -1,4 +1,55 @@
 package algorithm;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class FindFirstNonRepeatingCharInString {
-}
+
+    public static void main(String[] args) {
+        findNonRepeatedCharWithCollection2("enes");
+
+    }
+
+    public static void findNonRepeatedCharWithCollection(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            boolean unique = true;
+
+            for (int j = 0; j < str.length(); j++) {
+                if(i != j && str.charAt(i) == str.charAt(j)){
+                    unique = false;
+                    break;
+                }
+
+            }
+
+            if(unique){
+                System.out.println(str.charAt(i));
+                break;
+            }
+        }
+    }
+
+    public static void findNonRepeatedCharWithCollection2(String str) {
+
+       Map<Character, Integer> map = new HashMap<>();
+
+       for(int i = 0; i< str.length(); i++){
+           char ch = str.charAt(i);
+
+           if(map.containsKey(ch)){
+               map.put(ch, map.get(ch) + 1);
+           }else{
+               map.put(ch, 1);
+           }
+       }
+
+       for(Map.Entry<Character, Integer> entry : map.entrySet()){
+           if(entry.getValue()==1){
+               System.out.println(entry.getKey());
+               break;
+           }
+       }
+    }
+
+    }
--- src/main/java/algorithm/FindFirstRepeatedCharInString.java
+++ src/main/java/algorithm/FindFirstRepeatedCharInString.java
@@ -1,4 +1,42 @@
 package algorithm;
 
+import javax.swing.plaf.IconUIResource;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class FindFirstRepeatedCharInString {
+    public static void main(String[] args) {
+
+        firstChar2("mustafa");
+    }
+
+    public static void firstChar(String str) {
+
+        Set<Character> chars = new HashSet<>();
+
+        for (Character ch : str.toCharArray()) {
+            if (!chars.add(ch)) {
+                System.out.println(ch);
+            }
+        }
+    }
+
+    public static void firstChar2(String str) {
+
+        for (int i = 0; i < str.length()-1; i++) {
+            int count = 0;
+            for (int a = 1; a < str.length(); a++) {
+                if(str.charAt(i) == str.charAt(a)){
+                    count++;
+                }
+            }
+
+            if(count==2){
+                System.out.println("We found it");
+                System.out.println(str.charAt(i));
+            }
+        }
+
+    }
 }
